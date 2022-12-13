31ce99e594078481a544d6fa65a5e2902789a702
Sidneywasike <117063583+Sidneywasike@users.noreply.github.com>
2022-12-13 19:48:20 +0300

    Add files via upload

---
+++ children orphanage management system/home.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Children Orphanage Management System</title>
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>C</span>HILDREN<BR><span>O</span>RPHANAGE <span>S</span>YSTEM</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Services">Services</a></li>
+            <li><a href="#projects" data-after="Adaption Details">Adaption Details</a></li>
+	          <li><a href="adminLogin.php" data-after="Admin Login">Admin Login</a></li>
+            <li><a href="userLogin.php" data-after="User Login">User Login</a></li>
+            <li><a href="userReg.php" data-after="User Registration">User Registration </a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Welcome <span></span></h1>
+        <h1>To The <span></span></h1>
+        <h1>Children Orphanage Management System <span></span></h1>
+        <a href="#projects" type="button" class="cta">Adoption Details</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+        
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/cotton/64/000000/volunteering.png" /></div>
+          <h2>Adopt Child</h2>
+          
+        </div>
+        <div class="service-item">
+          
+          <h2>Make Donation</h2>
+         
+        </div>
+        <div class="service-item">
+          <h2>View Child</h2>
+         
+        </div>
+        <div class="service-item">
+          <h2>Be Volenteer</h2>
+       
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Adoption<span>Procedures</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>What is Adoption?</h1>
+            <h2>Child Welfare</h2>
+            
+          </div>
+          <div class="project-img">
+            <img src="./img/img num1.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Who can Adopt?</h1>
+            <h2>Child Welfare</h2>
+            <p>children are adopted by children fund authority through a certain program where
+			 the kids get regidtered and there background are followed by the authority and suppoert team</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img number2.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Minimum Age to Adopt?</h1>
+            <h2>Child Welfare</h2>
+            <p>children at the age of 0-17 in Kenya</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img num4.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Who Can be Adopted?</h1>
+            <h2>Child Welfare</h2>
+           <p> children adopted are those whose background is very unknown. those who parents left them,
+		   those who have a very humbled backgroung and their parents can't be able to meet their needs</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img num5.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Single People Can Adopt?</h1>
+            <h2>Child Welfare</h2>
+            <p>The program allows single people to recruit and adapt children</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img num6.jpg" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./img/img num3.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About the <span>system</span></h1>
+        <h2>Child Welfare</h2>
+        
+        <a href="#services" class="cta">View Services</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>Details</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Phone Number</h1>
+            <h2>+254 793717729</h2>
+            <h2>+254 769728626</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Email Address</h1>
+            <h2>sidney@gmail.com</h2>
+            <h2>sidneywasike@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Our Address</h1>
+            <h2>RUIRU Kiambu KENYA</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>C</span>HILDREN<span>O</span>RPHANAGE <span>S</span>YSTEM</h1>
+      </div>
+      <h2>You Can Follow Me On</h2>
+      <div class="social-icon">
+        <div class="social-item">
+        </div>
+        <div class="social-item">
+        </div>
+        <div class="social-item">
+        </div>
+        <div class="social-item">
+        </div>
+      </div>
+      <p>Copyright © 2022 Sidney Wasike Wafula</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+</html>
